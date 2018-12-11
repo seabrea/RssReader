d0c8919f67259186a79b61f8460bfaaf8b3800ac
Bob <hgdigm@gmail.com>
2018-12-11 09:58:47 +0800

    start project

---
+++ EasyRssReader/EasyRssReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyRssReader
+//
+//  Created by Bob on 2018/12/11.
+//  Copyright © 2018年 seabrea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
